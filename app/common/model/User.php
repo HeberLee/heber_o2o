25da6be0724be943eb49279f8115f950ff707f21
HeberLee <907008122@qq.com>
2018-02-11 20:23:29 +0800

    搭建中

--- app/common/model/User.php
+++ app/common/model/User.php
@@ -1,23 +1,16 @@
 <?php
 namespace app\common\model;
 use think\Model;
-use think\Request;
 
-class User extends Model{
-	protected $autoWriteTimestamp = true;
 
-	// public function getIdenticalUsername($username){
-	// 	// $data[
-	// 	// 	'username' => $username,
-	// 	// ];
-	// 	// $this->where($data)
-	// 	// 	->select();
-	// 	return $judge = $this->get(,['username'=>$username]);
-	// }
-	public function add($data){
+class User extends BaseModel{
+	public function add($data=[]){
+		$data['status'] = 1;
 		$id = $this->save($data);
 		return $id;
 	}
 
-
+	public function test(){
+		return 'yes';
+	}
 }
